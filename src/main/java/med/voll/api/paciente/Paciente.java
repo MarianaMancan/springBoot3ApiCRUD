f6d4d6247b8c57eb3d4246f27a034cdcab6dab31
Mariana Mancan <marianamancan@gmail.com>
2024-05-22 14:40:35 -0300

    first commit

---
+++ src/main/java/med/voll/api/paciente/Paciente.java
@@ -0,0 +1,57 @@
+package med.voll.api.paciente;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.endereco.Endereco;
+import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.Especialidade;
+
+@Table(name = "pacientes")
+@Entity(name = "paciente")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of= "id")
+public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String cpf;
+    private Boolean ativo;
+
+    @Embedded
+    private Endereco endereco;
+
+    public Paciente(DadosCadastroPaciente dados) {
+        this.ativo = true;
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.telefone = dados.telefone();
+        this.endereco = new Endereco(dados.endereco());
+        this.cpf = dados.cpf();
+    }
+
+    public void atualizarInformacoes(DadosAtualizacaoPaciente dados) {
+        if (dados.nome() != null)
+            this.nome = dados.nome();
+
+        if (dados.telefone() != null)
+
+            this.telefone = dados.telefone();
+
+        if (dados.endereco() != null)
+            endereco.atualizarInformacoes(dados.endereco());
+    }
+
+    public void inativar() {
+        this.ativo = false;
+    }
+}
